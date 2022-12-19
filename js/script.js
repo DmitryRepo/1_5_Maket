953298babc79d6854384ee62f41b3a38358174e0
Dmitry <cosmotronicsoul2@gmail.com>
2022-12-19 20:11:05 +0300

    1.5_pointer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const buttonOpen = document.querySelector('.cards-pointer');
 const cardsOpen = document.querySelector('.swiper-wrapper');
 let pointerOpen = document.querySelector('.span-pointer')
+let imageRotate = document.querySelector('.image-pointer')
 
 buttonOpen.addEventListener("click", buttonClick);
 
@@ -8,7 +9,9 @@ function buttonClick() {
     cardsOpen.classList.toggle('swiper-wrapper--open');
     if (cardsOpen.classList.contains('swiper-wrapper--open')) {
         pointerOpen.textContent="Скрыть";
+        imageRotate.classList.replace('image-pointer','image-pointer--rotate');
     } else {
         pointerOpen.textContent="Показать все";
+        imageRotate.classList.replace('image-pointer--rotate','image-pointer');
     }
 }
